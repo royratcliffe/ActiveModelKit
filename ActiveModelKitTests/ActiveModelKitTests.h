d112a192d53a335a4cfafb76a2c3f460d5555e2c
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2011-08-30 14:37:04 +0100

    ActiveModelKit, initially a Cocoa framework

---
+++ ActiveModelKitTests/ActiveModelKitTests.h
@@ -0,0 +1,13 @@
+//
+//  ActiveModelKitTests.h
+//  ActiveModelKitTests
+//
+//  Created by Roy Ratcliffe on 30/08/2011.
+//  Copyright 2011 Pioneering Software, United Kingdom. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ActiveModelKitTests : SenTestCase
+
+@end
